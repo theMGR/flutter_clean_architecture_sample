d1b6f4bfbf503c636511cd39a336559869852b4b
theMGR <87468246+theMGR@users.noreply.github.com>
2025-02-02 13:16:14 +0530

    Delete study_materials/Online-Shopping-App-SpringBoot- directory

--- study_materials/Online-Shopping-App-SpringBoot-/OnlineShopingApp/src/main/java/com/project/model/CategoryEnum.java
+++
@@ -1,6 +0,0 @@
-package com.project.model;
-
-public enum CategoryEnum {
-		
-	MOBILES,BOOKS,CLOTHS,FRUITS,VEGETABLES
-}
--- study_materials/Online-Shopping-App-SpringBoot-/OnlineShopingApp/src/main/java/com/project/service/UserLoginService.java
+++
@@ -1,7 +0,0 @@
-package com.project.service;
-
-import com.project.model.UserDTO;
-
-public interface UserLoginService {
-	public String userLogin(UserDTO userDto);
-}
